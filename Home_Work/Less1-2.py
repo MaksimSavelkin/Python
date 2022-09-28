e638fc5007e6fa03cc0f22d7c547cbc55ef89d99
msavelkin <msavelkin@gmail.com>
2022-09-28 11:39:52 +0300

    Верся для сдачи

--- Home_Work/Less1-2.py
+++ Home_Work/Less1-2.py
@@ -1,3 +1,11 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+x = False
+y = False
+z = False
+
+if not(x or y or z) == (not x) and (not y) and (not z):
+    print(True)
+else:
+    print(False)
--- Home_Work/Less1-5.py
+++ Home_Work/Less1-5.py
@@ -3,7 +3,7 @@
 #- A (3,6); B (2,1) -> 5,09
 #- A (7,-5); B (1,-1) -> 7,21
 
-from cmath import sqrt
+from math import sqrt
 
 
 x1 = int(input('Введите координату X первой точки: '))
@@ -11,8 +11,8 @@ y1 = int(input('Введите координату Y первой точки: '
 x2 = int(input('Введите координату X второй точки: '))
 y2 = int(input('Введите координату Y второй точки: '))
 
-import math
 
-dist = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+dist = round(sqrt((x1 - x2)**2 + (y1 - y2)**2),2)
 
 print(dist)
