16b57a647f1b142e5f22ca679e754079573226c5
savelkinms <savelkinms@gmail.com>
2022-10-20 14:30:07 +0300

    Домашняя работа Семинар 7

---
+++ Home_Work/Less7/Phone/convert.py
@@ -0,0 +1,12 @@
+import writewrite
+
+def convert(name: str):
+    with open(name, "r", encoding ="utf-8") as file:
+        for i in file:
+            surname, namename, phone, enough = i.split()
+            print('Перенесено', surname, namename, phone, enough)
+            write.init_surnameinit_surname(surnamesurname)
+            write.init_nameinit_name(namename)
+            write.init_phone(phone)
+            write.init_enough(enough)
+            write.write_from()
