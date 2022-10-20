925b86d16bbeb97d5635bf68bd263691a27846c6
savelkinms <savelkinms@gmail.com>
2022-10-20 14:02:30 +0300

    ДЗ Семинар 7

---
+++ Home_Work/Less7/call/model_div.py
@@ -0,0 +1,20 @@
+x = 0
+y = 0
+
+
+def init (a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+
+def div():
+    return x // y
+
+def div1():
+    return x / y
+
+
+def div2():
+    return x % y
---
+++ Home_Work/Less7/call/model_sqrt.py
@@ -0,0 +1,10 @@
+x = 0
+from math import sqrt
+
+def init(a):
+    global x
+    x = a
+
+
+def root():
+    return (sqrt(x), 2)
